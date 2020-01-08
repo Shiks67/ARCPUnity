2a018d9bf2c4afad3e4a92c63bbc5f64e3ef60c9
Arthur <arthur.salvat@gmail.com>
2020-01-08 10:50:46 +0100

    Initial commit

---
+++ Assets/AR-tour/Scripts/CameraPlayPauseBehaviour.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Vuforia;
+
+public class CameraPlayPauseBehaviour : MonoBehaviour
+{
+    public Camera ARCamera;
+    public Toggle CameraToggle;
+
+    private int previousCullingMask;
+    private CameraClearFlags previousClearFlags;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void PlayPauseCamera(bool CameraIsOn)
+    {
+        if(CameraIsOn)
+        {
+            /*CameraDevice.Instance.Start();
+            ObjectTracker imgTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+            imgTracker.Start();*/
+            Time.timeScale = 1;
+            ARCamera.clearFlags = previousClearFlags;
+            ARCamera.cullingMask = previousCullingMask;
+        }
+        else
+        {
+            /*CameraDevice.Instance.Stop();
+            ObjectTracker imgTracker = TrackerManager.Instance.GetTracker<ObjectTracker>();
+            imgTracker.Stop();*/
+            Time.timeScale = 0;
+            previousCullingMask = ARCamera.cullingMask;
+            previousClearFlags = ARCamera.clearFlags;
+            ARCamera.clearFlags = CameraClearFlags.Nothing;
+            ARCamera.cullingMask = 0;
+        }
+    }
+
+    public void ValueChanged()
+    {
+        PlayPauseCamera(CameraToggle.isOn);
+    }
+}
---
+++ Assets/AR-tour/Scripts/ItemLabelBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemLabelBehaviour : MonoBehaviour {
+
+    public Text Label;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Label.transform.LookAt(Camera.main.transform);
+        //TODO always turn the label to the Camera
+
+    }
+
+    public void UpdatePositionAndLabel(Transform labelPositionTransform, string label = "")
+    {
+        //TODO update position of the label and label value
+        
+    }
+}
